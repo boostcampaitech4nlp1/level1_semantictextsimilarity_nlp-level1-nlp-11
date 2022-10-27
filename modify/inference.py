d3c2f8eb684af8a7e88722accd860799ca7ce3ed
Jlnus <zmdk0902@naver.com>
2022-10-27 07:22:03 +0000

    fix dir

--- modify/inference.py
+++ modify/inference.py
@@ -175,23 +175,31 @@ if __name__ == '__main__':
     parser.add_argument('--max_epoch', default=1, type=int)
     parser.add_argument('--shuffle', default=True)
     parser.add_argument('--learning_rate', default=1e-5, type=float)
-    parser.add_argument('--train_path', default='train.csv')
-    parser.add_argument('--dev_path', default='dev.csv')
-    parser.add_argument('--test_path', default='dev.csv')
-    parser.add_argument('--predict_path', default='test.csv')
+    parser.add_argument('--train_path', default='../data/train.csv')
+    parser.add_argument('--dev_path', default='../data/dev.csv')
+    parser.add_argument('--test_path', default='../data/dev.csv')
+    parser.add_argument('--predict_path', default='../data/test.csv')
     args = parser.parse_args(args=[])
 
     # dataloader와 model을 생성합니다.
     dataloader = Dataloader(args.model_name, args.batch_size, args.shuffle, args.train_path, args.dev_path,
                             args.test_path, args.predict_path)
-    model = Model(args.model_name, args.learning_rate)
 
     # gpu가 없으면 'gpus=0'을, gpu가 여러개면 'gpus=4'처럼 사용하실 gpu의 개수를 입력해주세요
     trainer = pl.Trainer(gpus=1, max_epochs=args.max_epoch, log_every_n_steps=1)
 
-    # Train part
-    trainer.fit(model=model, datamodule=dataloader)
-    trainer.test(model=model, datamodule=dataloader)
+    # Inference part
+    # 저장된 모델로 예측을 진행합니다.
+    model = torch.load('model.pt')
+    predictions = trainer.predict(model=model, datamodule=dataloader)
 
-    # 학습이 완료된 모델을 저장합니다.
-    torch.save(model, 'model.pt')
+    # 예측된 결과를 형식에 맞게 반올림하여 준비합니다.
+    predictions = list(round(float(i), 1) for i in torch.cat(predictions))
+
+    # output 형식을 불러와서 예측된 결과로 바꿔주고, output.csv로 출력합니다.
+    output = pd.read_csv('../data/sample_submission.csv')
+    output['predicted_target'] = predictions
+    output.to_csv('output_diff.csv', index=False)
+    output['target'] = output['predicted_target']
+    del output['predicted_target']
+    output.to_csv('output.csv', index=False)
