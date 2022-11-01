f6e600af81742a8f015a1603c129289dbbcbaa43
siryuon <siryuon@gmail.com>
2022-11-01 14:57:56 +0000

    [ADD] 1101 experiment files

---
+++ 1101_inference.py
@@ -0,0 +1,188 @@
+import argparse
+
+import pandas as pd
+
+from tqdm.auto import tqdm
+
+import transformers
+import torch
+import torchmetrics
+import pytorch_lightning as pl
+
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, inputs, targets=[]):
+        self.inputs = inputs
+        self.targets = targets
+
+    def __getitem__(self, idx):
+        if len(self.targets) == 0:
+            return torch.tensor(self.inputs[idx])
+        else:
+            return torch.tensor(self.inputs[idx]), torch.tensor(self.targets[idx])
+
+    def __len__(self):
+        return len(self.inputs)
+
+
+class KfoldDataloader(pl.LightningDataModule):
+    def __init__(self, model_name, batch_size, shuffle, train_path, dev_path, test_path, predict_path, k:int=1, split_seed:int=42, num_splits:int=10):
+        super().__init__()
+        self.model_name = model_name
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.k = k
+        self.split_seed = split_seed
+        self.num_splits = num_splits
+        self.train_path = train_path
+        self.dev_path = dev_path
+        self.test_path = test_path
+        self.predict_path = predict_path
+
+        self.train_dataset = None
+        self.val_dataset = None
+        self.test_dataset = None
+        self.predict_dataset = None
+
+        self.tokenizer = transformers.AutoTokenizer.from_pretrained(model_name, max_length=512)
+        self.target_columns = ['label']
+        self.delete_columns = ['id']
+        self.text_columns = ['sentence_1', 'sentence_2']
+
+    def tokenizing(self, dataframe):
+        data = []
+        for idx, item in tqdm(dataframe.iterrows(), desc='tokenizing', total=len(dataframe)):
+            text = '[SEP]'.join([item[text_column] for text_column in self.text_columns])
+            outputs = self.tokenizer(text, add_special_tokens=True, padding='max_length', truncation=True)
+            data.append(outputs['input_ids'])
+        return data
+
+    def preprocessing(self, data):
+        data = data.drop(columns=self.delete_columns)
+
+        try:
+            targets = data[self.target_columns].values.tolist()
+        except:
+            targets = []
+        inputs = self.tokenizing(data)
+
+        return inputs, targets
+
+    def setup(self, stage='fit'):
+        if stage == 'fit':
+            train_data = pd.read_csv(self.train_path)
+            val_data = pd.read_csv(self.dev_path)
+            total_data = pd.concat([train_data, val_data], ignore_index=True)
+
+            total_input, total_targets = self.preprocessing(total_data)
+            total_dataset = Dataset(total_input, total_targets)
+
+            kf = KFold(n_splits=self.num_splits, shuffle=self.shuffle, random_state=self.split_seed)
+            all_splits = [k for k in kf.split(total_dataset)]
+
+            train_indexes, val_indexes = all_splits[self.k]
+            train_indexes, val_indexes = train_indexes.tolist(), val_indexes.tolist()
+
+            self.train_dataset = [total_dataset[x] for x in train_indexes]
+            self.val_dataset = [total_dataset[x] for x in val_indexes]
+        else:
+            test_data = pd.read_csv(self.test_path)
+            test_inputs, test_targets = self.preprocessing(test_data)
+            self.test_dataset = Dataset(test_inputs, test_targets)
+
+            predict_data = pd.read_csv(self.predict_path)
+            predict_inputs, predict_targets = self.preprocessing(predict_data)
+            self.predict_dataset = Dataset(predict_inputs, [])
+
+    def train_dataloader(self):
+        return torch.utils.data.DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=args.shuffle)
+
+    def val_dataloader(self):
+        return torch.utils.data.DataLoader(self.val_dataset, batch_size=self.batch_size)
+
+    def test_dataloader(self):
+        return torch.utils.data.DataLoader(self.test_dataset, batch_size=self.batch_size)
+
+    def predict_dataloader(self):
+        return torch.utils.data.DataLoader(self.predict_dataset, batch_size=self.batch_size)
+
+
+
+class Model(pl.LightningModule):
+    def __init__(self, model_name, lr):
+        super().__init__()
+        self.save_hyperparameters()
+
+        self.model_name = model_name
+        self.lr = lr
+
+        self.plm = transformers.AutoModelForSequenceClassification.from_pretrained(
+            pretrained_model_name_or_path=model_name, num_labels=1)
+        self.loss_func = torch.nn.L1Loss()
+
+    def forward(self, x):
+        x = self.plm(x)['logits']
+
+        return x
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = self.loss_func(logits, y.float())
+        self.log("train_loss", loss)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = self.loss_func(logits, y.float())
+        self.log("val_loss", loss)
+
+        self.log("val_pearson", torchmetrics.functional.pearson_corrcoef(logits.squeeze(), y.squeeze()))
+
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+
+        self.log("test_pearson", torchmetrics.functional.pearson_corrcoef(logits.squeeze(), y.squeeze()))
+
+    def predict_step(self, batch, batch_idx):
+        x = batch
+        logits = self(x)
+
+        return logits.squeeze()
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr)
+        return optimizer
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_name', default='monologg/koelectra-base-v3-discriminator', type=str)
+    parser.add_argument('--batch_size', default=16, type=int)
+    parser.add_argument('--max_epoch', default=20, type=int)
+    parser.add_argument('--shuffle', default=True)
+    parser.add_argument('--learning_rate', default=1e-5, type=float)
+    parser.add_argument('--train_path', default='../data/train.csv')
+    parser.add_argument('--dev_path', default='../data/dev.csv')
+    parser.add_argument('--test_path', default='../data/dev.csv')
+    parser.add_argument('--predict_path', default='../data/test.csv')
+    args = parser.parse_args(args=[])
+
+    dataloader = KfoldDataloader(args.model_name, args.batch_size, args.shuffle, args.train_path, args.dev_path,
+                            args.test_path, args.predict_path)
+
+    trainer = pl.Trainer(gpus=1, max_epochs=args.max_epoch, log_every_n_steps=1)
+
+    model = torch.load('model_1101_fold_10.pt')
+    #model = Model.load_from_checkpoint('./test-project/amj18xcs/checkpoints/epoch16-val_pearson0.9362.ckpt')
+    predictions = trainer.predict(model=model, datamodule=dataloader)
+    predictions = list(round(float(i), 1) for i in torch.cat(predictions))
+
+    output = pd.read_csv('../data/sample_submission.csv')
+    output['target'] = predictions
+    output.to_csv('1101_3.csv', index=False)
